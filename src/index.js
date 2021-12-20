637074b47cbe0be45391b3a08ebee7787c16f318
Emerson <emerson.pichardo@outllok.com>
2021-12-20 17:33:10 -0400

    Router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Result } from 'antd';
+
+import _Layout from './pages/_layout/_layout';
+import Home from './pages/home/home';
+import BlogEntriesList from './pages/blog/entries/list/blogEntriesList';
+import BlogEntriesDetails from './pages/blog/entries/details/blogEntriesDetails';
+
 import './index.css';
-import reportWebVitals from './reportWebVitals';
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    hola mundo
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<_Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blog/entries" element={<BlogEntriesList />} />
+          <Route path="blog/entries/{id}" element={<BlogEntriesDetails />} />
+
+          <Route path="*" element={<Result />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
